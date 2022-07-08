6b8e37af5d8703b2700f0cb95c839133332e4b5e
Saobby <101960076+Saobby@users.noreply.github.com>
2022-07-08 23:40:03 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,9 @@
-from flask import Flask, Response
+from flask import *
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
+    if path == "/venti":
+        return send_file("venti_origin.png")
     return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
